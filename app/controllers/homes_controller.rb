444f446e7017535a4d72ddf54e58774290768d6c
GitHubのユーザ名horikoshii <achiga9696@gmail.com>
2023-03-12 07:45:38 +0000

    top

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,16 @@
 class HomesController < ApplicationController
 
   def top
+    @platforms = Platform.all
+    @titles = Title.all
   end
 
+  def serch
+    @platforms = Platform.all
+    @titles = Title.all
+    if params[:platform_id]
+      @platform = Platform.find(params[:platform_id])
+      @titles = @platforms.titles
+    end
+  end
 end
